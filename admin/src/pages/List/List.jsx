24447655dd24841b2fbb51bbc9f9710338727045
Dhruv Chhabra <155481969+idhruvchhabra16@users.noreply.github.com>
2024-07-03 03:52:29 +0530

    Update List.jsx

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -4,7 +4,7 @@ import { url, currency } from '../../assets/assets'
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
-const List = () => {
+const List = ({url}) => {
 
   const [list, setList] = useState([]);
 
